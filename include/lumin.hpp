efc6df69c66242f79f145d0ae4cf9220bae7d89f
phil <phil@jensen.ancon-mamba.ts.net>
2025-12-02 23:17:40 -0500

    Update header file

---
+++ include/lumin.hpp
@@ -0,0 +1,7 @@
+#include "lumin/backend.hpp"
+#include "lumin/cpu_backend.hpp"
+#include "lumin/cuda_backend.hpp"
+#include "lumin/factory.hpp"
+#include "lumin/matrix.hpp"
+#include "lumin/mpi_backend.hpp"
+
